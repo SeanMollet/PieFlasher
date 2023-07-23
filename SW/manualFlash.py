d4bb6365165b6ebb5d033830de9e17ce894ba154
Sean Mollet <sean@malmoset.com>
2023-07-24 00:25:15 +0100

    Command line flashing

---
+++ SW/manualFlash.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import sys
+import os
+from utils import isfloat
+from power import setVoltage, maxPwrControlVoltage, disablePower, enablePower
+
+if len(sys.argv) < 3:
+    print("Usage: manualFlash.py <Filename> <Voltage>")
+    sys.exit(1)
+
+
+if not isfloat(sys.argv[2]):
+    print("Invalid voltage given. Aborting")
+    sys.exit(1)
+
+voltage = float(sys.argv[2])
+if voltage <1.2:
+    print("Minimum voltage is 1.2v. Aborting")
+    sys.exit(1)
+if voltage >5.0:
+    print("Maximum voltage is 5.0v. Aborting")
+    sys.exit(1)
+
+source = sys.argv[1]
+if not os.path.isfile(source):
+    print("Source file:",source,"not found. Aborting")
+    sys.exit(1)
+
+# Voltages above 3.4 must be controlled with PS_EN, since the FET for Output turns on automatically.
+# Can't go closed loop, but that's not really an issue with a 5V chip
+if voltage < maxPwrControlVoltage():
+    result = setVoltage(voltage,True,True)
+    if not result:
+        print("Unable to set requested voltage. Aborting.")
+        disablePower()
+        sys.exit(1)
+
+
+print("Press enter to flash")
+input()
+enablePower()
+
+print("Press enter when complete")
+input()
+disablePower()
+
