cdb9a5751755174521916c4893200dad23021cc4
Sean Mollet <sean@malmoset.com>
2023-07-24 16:55:36 -0500

    Make the worker client work. Impressive how well it does work..

--- SW/worker_client.py
+++ SW/worker_client.py
@@ -2,14 +2,31 @@
 import socketio
 
 sio = socketio.Client()
-sio.connect("http://10.23.0.5")
+sio.connect("http://10.23.0.10:5000")
 
 sio.emit("my_ping")
 
 
 @sio.on("my_pong")
-def my_pong(data):
-    print("Received a pong:", data)
+def my_pong():
+    print("Received a pong")
 
+    # sio.disconnect()
+    # exit(0)
+
+
+@sio.on("newFile")
+def newFile(fileName):
+    print("New file received:", fileName)
+
+
+@sio.on("shutdown")
+def shutdown():
+    print("Shutting down")
     sio.disconnect()
     exit(0)
+
+
+@sio.on("my_response")
+def my_response(data):
+    print("Data received:", data)
