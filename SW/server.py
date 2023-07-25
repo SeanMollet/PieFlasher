c10457f9822198ce05c3980735477dce3108c814
Sean Mollet <sean@malmoset.com>
2023-07-25 06:12:58 -0500

    WiP: web interface for the server

--- SW/server.py
+++ SW/server.py
@@ -1,17 +1,79 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template, session
+from flask import (
+    Flask,
+    render_template,
+    session,
+    request,
+    send_from_directory,
+    send_file,
+)
+from werkzeug.utils import secure_filename
 from flask_socketio import SocketIO, emit
 import os
 from pathlib import Path
+from tempfile import TemporaryDirectory
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret!"
 socketio = SocketIO(app)
+filesPath = "data/files"
+
+
+@app.route("/websocket_test.html")
+def websocket_test():
+    return render_template("websocket_test.html")
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("status.html")
+
+
+@app.route("/status/")
+def status():
+    return render_template("status.html")
+
+
+@app.route("/logs/")
+def logs():
+    return render_template("logs.html")
+
+
+@app.route("/upload/")
+def upload():
+    return render_template("upload.html")
+
+
+@app.route("/configure/")
+def configure():
+    return render_template("configure.html")
+
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/images/<path:path>")
+def send_image(path):
+    return send_from_directory("images", path)
+
+
+@app.route("/files/<name>")
+def getFile(name):
+    print("Attempting to download file:", name)
+    return "Here's the file!"
+
+
+@app.route("/files/upload", methods=["POST"])
+def postFile():
+    print("Attemped to upload a file")
+    f = request.files["file"]
+    filename = secure_filename(f.filename)
+    fullPath = os.path.join(filesPath, filename)
+    f.save(fullPath)
+    print("Saved file to:", fullPath)
+    return ""
 
 
 @socketio.event
@@ -63,4 +125,9 @@ def my_ping():
 
 
 if __name__ == "__main__":
+    fileTempPath = TemporaryDirectory()
+    app.config["TEMPLATES_AUTO_RELOAD"] = True
+    app.config["UPLOAD_FOLDER"] = fileTempPath
+    # 256 MB should be more than enough
+    app.config["MAX_CONTENT_PATH"] = 256 * 1024 * 1024
     socketio.run(app, host="0.0.0.0")
