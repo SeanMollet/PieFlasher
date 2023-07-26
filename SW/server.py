02b700d4a6de9edef0f669b45768358850348b74
Sean Mollet <sean@malmoset.com>
2023-07-26 16:26:41 -0500

    Formallly uuse the cwd as the base

--- SW/server.py
+++ SW/server.py
@@ -29,7 +29,7 @@ from operator import itemgetter
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret!"
 socketio = SocketIO(app)
-filesPath = Path("data", "files")
+filesPath = Path(os.getcwd(), "data", "files")
 print("Using", os.path.abspath(filesPath), "as data path")
 if not os.path.isdir(filesPath):
     filesPath.mkdir(parents=True)
