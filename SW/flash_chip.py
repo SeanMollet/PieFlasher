0dc03eb27ed6ea8d1261a355441afc5df5fd08c4
Sean Mollet <sean@malmoset.com>
2024-03-26 22:00:41 +0000

    Add another Winbond chip

--- SW/flash_chip.py
+++ SW/flash_chip.py
@@ -34,6 +34,7 @@ class flashChip:
     CMD_Page_Program_4 = 0x12  # A31-A17 A23-A16  A15-A8   A7-A0   (D7-D0) (Next byte)    (continuous)
     KnownGoodIDs = [
         (0xEF, 0x6018, "Winbond", "W25Q128JW", 1.8, 20, 4 * 1024, 4096),
+        (0xEF, 0x7018, "Winbond", "W25Q128JV", 3.3, 20, 4 * 1024, 4096),
         (0xC2, 0x2019, "Macronix", "MX25L25645G", 3.3, 20, 4 * 1024, 4096),
     ]
 
